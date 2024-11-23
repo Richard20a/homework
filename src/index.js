cc66791addfa9ffce82847e323cee743889b3ca5
Richard20a <mrichard932@gmail.com>
2024-11-23 19:52:06 +0100

    step 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // CODE HERE
 function greetings(){
-    
+    console.log("Hello, my friend");
 }
 
 module.exports = greetings;
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@
 const greetings = require('../src/index');
 
 describe("Greetings input handling test", () => {
-    it("No input", () => {
+    it("No input: writes 'my friend' after the greeting", () => {
         console.log = jest.fn();
         greetings();
         expect(console.log).toHaveBeenCalledWith("Hello, my friend");
