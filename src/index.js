837d19bb81fb95d81de5e1dc2e350c1d3f223fbc
Richard20a <mrichard932@gmail.com>
2024-11-23 20:06:14 +0100

    step 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // CODE HERE
-function greetings(){
-    console.log("Hello, my friend");
+function greetings(...name){
+    if(names.length === 0){
+        console.log("Hello, my friend");
+    }
+    else{
+        console.log("Hello "+ name);
+    }
 }
 
 module.exports = greetings;
